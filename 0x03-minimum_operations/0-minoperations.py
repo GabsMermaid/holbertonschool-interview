3637b692ba36519450916a948af40c83ad85d27e
Gabriela Martinez <g.martinez.rodz@gmail.com>
2022-05-11 11:28:46 -0400

    Renaming Directory as given in project

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -5,6 +5,7 @@ Minimum Operations
 """
 
 def minOperations(n):
+    
     """
     In a text file, there is a single character H. Your text editor can execute only two operations in this file:
     Copy All and Paste. Given a number n, write a method that calculates the fewest number of operations needed
